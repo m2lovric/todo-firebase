40b2cdd71e42dd602d43c9960faf1bbaae463568
m2lovric <m2lovric@gmail.com>
2019-04-03 23:13:42 +0200

    setup

---
+++ app.js
@@ -0,0 +1,17 @@
+let input = document.querySelector('input');
+let list = document.querySelector('#item-list');
+let submit = document.querySelector('button');
+let value;
+
+submit.addEventListener('click',e => {
+    e.preventDefault();
+    value = input.value;
+    let li = document.createElement('li');
+    let text = document.createTextNode(value);
+    li.append(text);
+    list.append(li);
+    input.value = '';
+});
+
+
+
