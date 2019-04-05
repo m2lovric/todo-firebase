4107f2071c6aa0028ccd9830583d6cb34395c6cb
matteo <m2lovric@gmail.com>
2019-04-05 19:59:44 +0200

    app update

--- app.js
+++ app.js
@@ -1,35 +1,54 @@
-let input = document.querySelector('input');
-let list = document.querySelector('#item-list');
-let submit = document.querySelector('button');
-let value;
+const list = document.querySelector('#item-list');
+const submit = document.querySelector('button');
+const form = document.querySelector('#add-form')
 
 let renderTasks = (doc) => {
     let li = document.createElement('li');
-    li.setAttribute('data.id', doc.id);
     let task = document.createElement('span');
     let importance = document.createElement('span');
-    importance.setAttribute('class', 'rating');
+    let cross = document.createElement('div');
 
+    li.setAttribute('data-id', doc.id);
+    importance.setAttribute('class', 'rating');
     task.textContent = doc.data().task;
     importance.textContent = doc.data().importance;
+    cross.textContent = 'x';
+
     li.appendChild(task);
     li.appendChild(importance);
+    li.appendChild(cross);
     list.append(li);
+
+    //delete data
+    cross.addEventListener('click', e => {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        console.log(id);
+        db.collection('tasks').doc(id).delete();
+    })
 }
 
-submit.addEventListener('click',e => {
-    e.preventDefault();
-    value = input.value;
-    
-    input.value = '';
-});
-
 //firestore
-db.collection('tasks').get().then((s) => {
-    s.docs.forEach(el => {
-        renderTasks(el);
+db.collection('tasks').orderBy('importance', 'desc').onSnapshot(s => {
+    let changes = s.docChanges();
+    changes.forEach(change => {
+        if(change.type == 'added'){
+            renderTasks(change.doc);
+        }else if(change.type == 'removed'){
+            let li = list.querySelector(`[data-id="${change.doc.id}"]`);
+            list.removeChild(li);
+        }
     });
-});
+})
 
-
+//saving data
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    db.collection('tasks').add({
+        importance: parseInt(form.num.value),
+        task: form.txt.value
+    });
+    form.num.value = '';
+    form.txt.value = '';
+})
 
